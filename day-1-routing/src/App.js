4fd7f11f2c33bc14f6f2516aba4d3df716dcbd33
Akshayy <mote.akshay.18et1011@gmail.com>
2024-07-26 11:19:44 +0530

    ..

--- day-1-routing/src/App.js
+++ day-1-routing/src/App.js
@@ -1,4 +1,5 @@
-import { Route } from 'react-router-dom';
+import {BrowserRouter, Route } from 'react-router-dom';
+
 import { Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home';
@@ -9,15 +10,17 @@ import Register from './Components/Register';
 
 function App() {
   return (
+  <BrowserRouter>
     <div className="App">
       <Routes>
         <Route path='/' element={<Home/>}/> 
+        <Route path='/register' element={<Register/>}/> 
         <Route path='/mens' element={<Mens/>}/> 
         <Route path='/profile' element={<Profile/>}/> 
         <Route path='/login' element={<Login/>}/> 
-        <Route path='/register' element={<Register/>}/> 
       </Routes>
     </div>
+  </BrowserRouter>
   );
 }
 
