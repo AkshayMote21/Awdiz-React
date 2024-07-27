a5dc06ac3db66c897d3c6ca42181d682cfdeb4c6
Akshayy <mote.akshay.18et1011@gmail.com>
2024-07-27 11:40:32 +0530

    updates...

--- day-4/src/30-05/UserDetails.jsx
+++ day-4/src/30-05/UserDetails.jsx
@@ -3,6 +3,13 @@ import { useParams } from 'react-router-dom'
 
 export const UserDetails = () => {
     const {username} = useParams();
+
+//   useEffect(() => {
+//     //  use username and call api to fetch product details
+//     if(username){
+//         // call api
+//     }
+//   }, [username]);
   return (
     <div>
         <h1>UserDetails - {username}</h1>
